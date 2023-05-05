264518085cfdb16bb9b8d17787a94607cd577108
汉广 <serfend@foxmail.com>
2023-05-05 18:28:09 +0800

    Initial commit

---
+++ PingDetector/IpConfig.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkApi.Network.PingDetector
+{
+	public class IpConfig
+	{
+		public IpConfig()
+		{ }
+
+		public IpConfig(string ip, bool enable, string port, string? name = null, string? description = null)
+		{
+			Ip = ip;
+			Enable = enable;
+			Port = port;
+			Name = name;
+			Description = description;
+		}
+
+		public string? Ip { get; set; }
+		public bool Enable { get; set; }
+		public string? Port { get; set; }
+		public string? Name { get; set; }
+		public string? Description { get; set; }
+	}
+}
---
+++ PingDetector/PingSuccessEventArgs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net.NetworkInformation;
+
+namespace Project.Core.Protector.BLL.Network.PingDetector
+{
+	public class PingSuccessEventArgs : EventArgs
+	{
+		public PingSuccessEventArgs(PingReply reply,string host)
+		{
+			Reply = reply;
+            Host = host;
+        }
+
+		public PingReply Reply { get; }
+        public string Host { get; }
+    }
+}
